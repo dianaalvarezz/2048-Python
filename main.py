406fcdbc8fa9e54649d9ea923d977bfd5b7359a5
Diana Alvarez <143985084+dianaalvarezz@users.noreply.github.com>
2024-03-03 12:52:08 -0600

    Update main.py
    Get number of lines function is now complete

--- main.py
+++ main.py
@@ -38,34 +38,35 @@ def get_number_of_lines():
     # until they give valid amount
     while True:
         
-        # 
+        # Promits the user to enter the number of lines to bet one 1 - 3 
         lines = input("Enter the number of lines to bet on (1-" + str(MAX_LINES) + ")? ")
 
         # Makes sure amount entered is actually a number
-        if amount.isdigit():
+        if lines.isdigit():
 
             # Coverts string entered into a int
-            amount = int(amount)
+            lines = int(lines)
 
-            # Checks if amount is greater than zero
-            # If it is then function breaks out and returns amount
-            if amount > 0:
+            # Checks to see if amount of lines is greater than 1 but less than
+            # Max Lines (3)
+            if 1 <= lines <= MAX_LINES:
                 break
             else:
 
-                # Prints if amount is less than zero to alert user
-                print("Amount must be greater than 0.")
+                # Prints if bumber of lines is not between 1 - MAX_LINES (3)
+                print("Enter a valid number of lines")
         else:
 
             # Prints if user does not enter a number
             print("PLease enter a number.")
 
-    return amount
+    return lines
 
-
+def 
 def main():
 # Calling deposit function
     balance = deposit()
+    lines = get_number_of_lines()
 
 main()
 
